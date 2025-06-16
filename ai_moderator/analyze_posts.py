bb6e76596b9fd5373ce7922dd25445fbecdab120
thedataninja1786 <chrissolomou1786@gmail.com>
2025-06-16 19:29:28 +0300

    Added analyze posts.py

---
+++ ai_moderator/analyze_posts.py
@@ -0,0 +1,75 @@
+from dataloader.load_data import DataLoader
+from typing import List, Dict, Any, Tuple, Optional
+from ai_moderator.chatbot import AIModerator
+
+
+class PostAnalyzer:
+    def find_new_posts(
+        self, loader: DataLoader, post_limit: int
+    ) -> List[Dict[str, Any]]:
+        """Retrieves the most recent posts that have not yet been analyzed."""
+
+        try:
+            q = f"""
+            WITH recent_posts AS (
+                SELECT
+                    id,
+                    title,
+                    author,
+                    flair,
+                    selftext,
+                    created_utc
+                FROM posts
+                ORDER BY created_utc DESC
+                LIMIT {post_limit}
+            )
+            SELECT *
+            FROM recent_posts
+            WHERE id NOT IN (
+                SELECT id
+                FROM posts_ai_analysis
+                WHERE category IS NOT NULL
+                ORDER BY created_utc DESC
+                LIMIT {post_limit}
+            );
+            """
+            posts_df = loader.query_table(q)
+            return posts_df.to_dict("records")
+        except Exception as e:
+            print(f"{self.__class__.__name__} - {self.find_new_posts.__name__}")
+            print(f"[ERROR] occurred {e}")
+            return []
+
+    def process_posts(
+        self, moderator: AIModerator, posts: List[Dict[str, Any]]
+    ) -> List[Tuple[str, str, str, str, str, Optional[str], Optional[str], Any]]:
+        """
+        Fetch ai analysis for new post and return a row-tuple for upsert.
+        """
+        res = []
+
+        for post in posts:
+            try:
+                analysis = moderator.generate_sentiment(
+                    flair=post["flair"],
+                    title=post["title"],
+                    selftext=post["selftext"],
+                )
+                res.append(
+                    (
+                        post["id"],
+                        post["title"],
+                        post["author"],
+                        post["flair"],
+                        post["selftext"],
+                        analysis.get("category"),
+                        analysis.get("reasoning"),
+                        post["created_utc"],
+                    )
+                )
+            except Exception as e:
+                # TODO log properly in side-outputs
+                print(f"{self.__class__.__name__} - {self.process_posts.__name__}")
+                print(f"[ERROR] occurred when processing post {post['id']}: {e}")
+
+        return res
--- ai_moderator/chatbot.py
+++ ai_moderator/chatbot.py
@@ -1,25 +1,14 @@
-from dotenv import load_dotenv
-import os
 import json
 from openai import OpenAI
 from typing import Optional, Dict
-import asyncio
-
-load_dotenv()
-api_key = os.getenv("API_KEY").strip()
-
-if not api_key:
-    raise ValueError("No API key found in environment variables")
-
-client = OpenAI(api_key=api_key)
 
 class AIModerator:
-    def __init__(self,client:OpenAI):
+    def __init__(self, client: OpenAI):
         self.client = client
 
-    def _generate_prompt(self,flair: str, title: str, body: str) -> str:
+    def _generate_prompt(self, flair: str, title: str, selftext: str) -> str:
         """Generates a prompt for categorizing a Reddit post into predefined categories
-           with justification, formatted for an AI moderator assistant."""
+        with justification, formatted for an AI moderator assistant."""
 
         prompt = (
             "You are an AI moderator assistant for the subreddit r/WorldofTanks.\n"
@@ -37,27 +26,25 @@ class AIModerator:
             f"Here is the post:\n"
             f"Flair: {flair}\n"
             f"Title: {title}\n"
-            f"Body: {body}\n\n"
+            f"Body: {selftext}\n\n"
             "Respond with the best matching category and a short one-sentence justification. "
             "Respond in a JSON (OMIT MARKDOWN) in dictionary format with keys 'category' and 'reasoning'."
         )
         return prompt
 
-
-    async def generate_sentiment(self, flair:Optional[str],title:str,body:Optional[str]) -> Dict[str, str]:
+    def generate_sentiment(
+        self, flair: Optional[str], title: str, selftext: Optional[str]
+    ) -> Dict[str, str]:
         """
-        Asynchronously generates sentiment analysis results for a given Reddit post's flair, title, and body using an AI model.
+        Generates sentiment analysis results for a given Reddit post's flair, title, and body using an AI model.
         """
-        
 
-        if not body and not flair:
-            return {} 
+        if not selftext and not flair:
+            return {}
 
-        prompt = self._generate_prompt(flair = flair, title = title, body = body)
-        print(prompt)
-        
+        prompt = self._generate_prompt(flair=flair, title=title, selftext=selftext)
+
         try:
-            # acreate if the async variant 
             response = self.client.chat.completions.create(
                 messages=[
                     {
@@ -73,19 +60,6 @@ class AIModerator:
 
         except Exception as e:
             print(
-                f"{self.generate_sentiment.__name__} - generate sentiment failed: {e}"
+                f"{self.generate_sentiment.__name__} [ERROR] - generate sentiment failed: {e}"
             )
             return {}
-
-
-async def main():
-    mod = AIModerator(client)
-    result = await mod.generate_sentiment(
-        flair="Discussion",
-        title="How to improve my tank gameplay?",
-        body="I can’t penetrate tier 8 heavy tanks…"
-    )
-    print(type(result))
-
-if __name__ == "__main__":
-    asyncio.run(main())
